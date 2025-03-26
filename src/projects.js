14af9699b7dab5ba9322e2b32f8dbe932e7f5e8d
Adam <105685257+AdamAjiLangit@users.noreply.github.com>
2025-03-26 12:08:32 +0700

    add : [AD] project

--- src/projects.js
+++ src/projects.js
@@ -3,7 +3,7 @@ const projects = [
         id: 0,
         slug: "project-1",
         title: "Neo Brutalism",
-        description: "This is a project description.",
+        description: "Neo Brutalism style. Striking, edgy art.",
         images: [
             "https://img.freepik.com/premium-vector/neo-brutalism-modern-poster-cover-with-wavy-distorted-statue-antique-goddess-psychedelic_119217-6559.jpg?w=740",
             "https://img.freepik.com/premium-vector/trendy-neo-brutalism-grunge-poster-with-retro-offset-halftone-hand-element-psychedelic-brutal_119217-6557.jpg?w=740",
@@ -14,11 +14,23 @@ const projects = [
         id: 1,
         slug: "project-2",
         title: "Ratio",
-        description: "This is a project description.",
+        description: "Greek god.",
         images: [
             "https://img.freepik.com/free-vector/online-dating-matching-greek-god-statue-aesthetic-social-media-post_53876-114651.jpg?t=st=1742864703~exp=1742868303~hmac=629916f47d62093ef40f35864b998bb48c060a9b37a712d93c0451870714f5a9&w=740",
             "https://img.freepik.com/free-vector/social-media-addiction-summer-vibes-aesthetic-post_53876-117217.jpg?t=st=1742860833~exp=1742864433~hmac=b6ae1fb92d3bcbe95ca8cb8799e6246b7f1e6699459ffd9666c2ed9b165bbcf8&w=740",
             "https://img.freepik.com/free-vector/cyber-bullying-mixed-media-crying-greek-god-statue-aesthetic-post_53876-100469.jpg?t=st=1742860854~exp=1742864454~hmac=259461a7f5bae24acc810d4166441dd2a73ec38f504838027f8bd42d20c3447d&w=740"
+        ],
+    },
+    {
+        id: 2,
+        slug: "project-3",
+        title: "The Bloom Within",
+        description: "Unveiling Inner Beauty.",
+        images: [
+            "https://i.pinimg.com/736x/e0/2b/1c/e02b1c2bf10e6ac149d40a4d96d673ff.jpg",
+            "https://i.pinimg.com/736x/bd/e2/34/bde23403be3096be51f606192b4b65fc.jpg",
+            "https://i.pinimg.com/736x/e6/26/83/e6268385f8e375249b71bcd9568f363b.jpg",
+            "https://i.pinimg.com/736x/95/01/f6/9501f6b95d91117fdbad3e0f5d8d4490.jpg",
         ],
     }
 ]
